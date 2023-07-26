6e8d13616c879d7cb27dd388cab04a2bd24209e9
Felix Roos <flix91@gmail.com>
2023-07-26 23:15:52 +0200

    superdirt helper

---
+++ superdirt.mjs
@@ -0,0 +1,34 @@
+import OSC from "osc-js";
+
+export function connect() {
+  const options = {
+    type: "udp4", // @param {string} 'udp4' or 'udp6'
+    open: {
+      host: "localhost", // @param {string} Hostname of udp server to bind to
+      port: 41234, // @param {number} Port of udp server to bind to
+      exclusive: false, // @param {boolean} Exclusive flag
+    },
+    send: {
+      host: "localhost", // @param {string} Hostname of udp client for messaging
+      port: 57120, // @param {number} Port of udp client for messaging
+    },
+  };
+
+  const osc = new OSC({ plugin: new OSC.DatagramPlugin(options) });
+
+  osc.on("open", () => {
+    console.log("osc connected");
+  });
+
+  osc.on("*", (message) => {
+    console.log(message.args);
+  });
+
+  osc.open();
+  return (controls) => {
+    const entries = Object.entries(controls).flat();
+    console.log("entries", entries);
+    const message = new OSC.Message("/dirt/play", ...entries);
+    osc.send(message);
+  };
+}
