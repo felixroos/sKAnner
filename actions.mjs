79da5e1f0b1a03d59a50bbc6d0d80a52cc6aab73
Felix Roos <flix91@gmail.com>
2023-07-30 14:52:09 +0200

    killall before open barcode

--- actions.mjs
+++ actions.mjs
@@ -1,4 +1,4 @@
-import open from "open"; // helper to open a url in the default browser
+import openURL from "open"; // helper to open a url in the default browser
 import { exec } from "node:child_process";
 import { connect } from "./superdirt.mjs";
 
@@ -19,21 +19,74 @@ const cmd = (command) => {
   });
 };
 
+// prequisite:
+// Google Chrome needs to be the default Browser
+// Visual Studio Code needs the rights to control keyboard input
+// just enter
+// osascript -e 'tell application "System Events" to keystroke return using control down'
+// into the vscode terminal and you should get a prompt...
+
+const ctrlEnter = () => {
+  cmd(
+    `osascript -e 'tell application "System Events" to keystroke return using control down'`
+  );
+};
+
+const killall = () => {
+  cmd('killall "Google Chrome"');
+  cmd('killall "SuperCollider"');
+};
+
+const sc = async (n) => {
+  killall();
+  await sleep(200);
+  cmd(`SuperCollider ./sc/sc${n}.sc`);
+  await sleep(5000);
+  ctrlEnter();
+};
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const open = async (url) => {
+  killall();
+  await sleep(500);
+  openURL(url);
+  await sleep(2000);
+  ctrlEnter();
+};
+
 const actions = {
   // st actions will only work if SuperCollder is in PATH!
-  sc1: () => cmd("SuperCollider ./sc/sc1.sc"),
-  sc2: () => cmd("SuperCollider ./sc/sc2.sc"),
-  sc3: () => cmd("SuperCollider ./sc/sc3.sc"),
-  sc4: () => cmd("SuperCollider ./sc/sc4.sc"),
-  sc5: () => cmd("SuperCollider ./sc/sc5.sc"),
-  sc6: () => cmd("SuperCollider ./sc/sc6.sc"),
-  sc7: () => cmd("SuperCollider ./sc/sc7.sc"),
-  sc8: () => cmd("SuperCollider ./sc/sc8.sc"),
-  sc9: () => cmd("SuperCollider ./sc/sc9.sc"),
-  st1: () => open("https://strudel.tidalcycles.org/?7VoIcReDMO-M"),
-  st2: () => open("https://strudel.tidalcycles.org/?dDsZmIqDmCXf"),
-  st3: () => open("https://strudel.tidalcycles.org/?kl_UEvr3rc07"),
-  st4: () => open("https://strudel.tidalcycles.org/?VUbgKpHsf-G9"),
+  sc1: () => sc(1),
+  sc2: () => sc(2),
+  sc3: () => sc(3),
+  sc4: () => sc(4),
+  sc5: () => sc(5),
+  sc6: () => sc(6),
+  sc7: () => sc(7),
+  sc8: () => sc(8),
+  sc9: () => sc(9),
+  // using long urls instead of short ones, we can skip loading the code from the database..
+  //st1: () => open("https://strudel.tidalcycles.org/?7VoIcReDMO-M"),
+  st1: () =>
+    open(
+      "https://strudel.tidalcycles.org/?bihydW4oOCkucGFsaW5kcm9tZSgpKQouY2hvcmQoIjxEbTdbRzcgQTddPiIpCi52b2ljaW5nKCkgIC5zKCJzaW5lIikKLmFkZC5zcXVlZXplKG4oIjAgLjI1IgopKS5yb29tKDEpLmNsaXAoY29zaW5lCi5yYW5nZSguMSwyKSAuc2xvdyg0KSkKLmVjaG9XaXRoKDQsLjEsKHgsbik9Pgp4IC5hZGQobm90ZShuICogLjI1KSkpCi5zbG93KDQpLnB1bmNoY2FyZCh7fSk%3D"
+    ),
+  // st2: () => open("https://strudel.tidalcycles.org/?dDsZmIqDmCXf"),
+  st2: () =>
+    open(
+      "https://strudel.tidalcycles.org/#cygiYmQgcmltLCBbfiBoaCoyXSIpCi5iYW5rKCAiUm9sYW5kVFI5MDkiKQouZml0KCkuanV4KHJldikuY2h1bmsKKDQsIG11bCggc3BlZWQoMC41KSkpCi5yZXN0YXJ0KGA8eEA3IHgoNSw4KQo%2BYCkuanV4KHggPT4geCAuZ2FpbigKcmFuZCkucmV2KCkpLnN0YWNrKHMoCiJzYXd0b290aCIpLmV1Y2xpZFJvdAooMyw4LDQpLmxwZig4MDApLmxwcSgKc2F3LnJhbmdlKDEsOSkpKS8vIGZy"
+    ),
+  //st3: () => open("https://strudel.tidalcycles.org/?kl_UEvr3rc07"),
+  st3: () =>
+    open(
+      "https://strudel.tidalcycles.org/#bihydW4oMTIpKS5jaG9yZCgiRCIpCi52b2ljaW5nKCkucygicGlhbm8iKQouYWRkKCAiPC0xIDAgMSAwPi80IikKLmp1eChyZXYpLmxwcSg0KS5scGYoCnNhdy5yYW5nZSgxMDAsNDAwMCkpLgpyYXJlbHkoYWRkKG5vdGUoMTIpKSkKLmFkZChub3RlKHBlcmxpbi5kaXYoCjIpKSkubGFzdE9mKDQscmV2KS8vLwouc3ViKG5vdGUoIjEyWzAgMl0iKSkKLnNsb3coMikgIC5yb29tKHNpbmUpCi5waWFub3JvbGwoe2ZvbGQ6MSB9KQ%3D%3D"
+    ),
+  //st4: () => open("https://strudel.tidalcycles.org/?VUbgKpHsf-G9"),
+  st4: () =>
+    open(
+      "https://strudel.tidalcycles.org/#bGV0IGNob3JkcyA9Y2hvcmQoYDwKQ143IEZeNyBFYl43IEFiXjc%2BYCkKc3RhY2sobihydW4oMTIpLnNsb3cKKDIpKS5qdXgoeD0%2BeC5yZXYoKS4KbGF0ZSguNSkpLnNldChjaG9yZHMKKS5vZmZzZXQoYDwwIDEgMiAxIDAgCi0xIC0yIC0xPi8yYC5hZGQoMSkpCi52b2ljaW5nKCkuY2xpcCgiMiIpCi5kZWxheSguNSkuZ2FpbihzaW5lCi5yYW5nZSguMiwxKS5mYXN0KC45CikpLGNob3Jkcy5yb290Tm90ZXMoCjIpKS5waWFubygpLmFkZChub3RlCihwZXJsaW4ucmFuZ2UoMCwuMjUpCikpLnB1bmNoY2FyZCgpIC8vIDwz"
+    ),
   hd1: () => open("https://hydra.ojack.xyz/?sketch_id=Pr16zeXkfaLUd5Kd"),
   hd2: () => open("https://hydra.ojack.xyz/?sketch_id=ExeOTZVE9hrn6IIW"),
   hd3: () => open("https://hydra.ojack.xyz/?sketch_id=vcWMV3vi1ykmd9U1"),
